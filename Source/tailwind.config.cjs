f4a2322a08911a6151c224e93c2c7e54f53d384f
phucthaii1820 <62050136+phucthaii1820@users.noreply.github.com>
2021-11-27 00:10:59 +0700

    add partials card

--- Source/tailwind.config.cjs
+++ Source/tailwind.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        'montserrat':'Montserrat',
+      }
+    },
   },
   variants: {
     extend: {
