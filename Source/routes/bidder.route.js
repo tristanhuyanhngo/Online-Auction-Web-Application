70b5a7ec622990cd3fd8b46a9bb3d45d30fd37c1
Thanh Hoang <thanhhoang4869@gmail.com>
2021-11-28 23:19:26 +0700

    create bidder's general setting UI

--- Source/routes/bidder.route.js
+++ Source/routes/bidder.route.js
@@ -3,9 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('bidder/account');
+    res.render('bidder/edit_info',{
+        layout: 'account.handlebars'
+    });
 });
-
 
 // router.get('/register', (req, res) => {
 //     res.render('register');
