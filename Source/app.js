80bb03d1f3154d32c042894018ded1a7118e0645
phucthaii1820 <62050136+phucthaii1820@users.noreply.github.com>
2021-11-28 14:46:56 +0700

    add page product

--- Source/app.js
+++ Source/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
+import { engine, create } from 'express-handlebars';
 import indexRoute from './routes/index.js'
+import productRoute from './routes/product.js'
 import path from 'path'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -9,6 +10,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
+const hbs = create({
+    helpers: {
+        times(n, block) {
+            var accum = '';
+            for (var i = 0; i < n; ++i)
+                accum += block.fn(i);
+            return accum;
+        }
+    }
+});
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -18,5 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/', indexRoute)
+app.use('/product', productRoute)
+
 
 app.listen(3000);
