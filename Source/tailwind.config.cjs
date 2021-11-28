59c094a255f62aa552402a839f5028f2eb88682b
Thanh Hoang <thanhhoang4869@gmail.com>
2021-11-29 01:52:20 +0700

    add custom color to tailwinđ.config.js

--- Source/tailwind.config.cjs
+++ Source/tailwind.config.cjs
@@ -5,6 +5,28 @@ module.exports = {
     extend: {
       fontfamily: {
         'montserrat':'Montserrat',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        blue: {
+          'light': '#85d7ff',
+          DEFAULT: '#4682B4',
+          royal: '#4169E1',
+          dark: '#009eeb',
+        },
+        pink: {
+          light: '#ff7ce5',
+          DEFAULT: '#ff49db',
+          dark: '#ff16d1',
+        },
+        gray: {
+          darkest: '#1f2d3d',
+          dark: '#3c4858',
+          DEFAULT: '#c0ccda',
+          light: '#e0e6ed',
+          lightest: '#f9fafc',
+        }
       }
     },
   },
