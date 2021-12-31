ae14c3d1003de7a9cdc064bb695dbf1cb88f42e4
Huy Anh Ngo <tristanhuyanhngo@gmail.com>
2021-12-31 17:32:52 +0700

    Seller Upload Commit
    Already Done

--- Source/routes/seller.route.js
+++ Source/routes/seller.route.js
@@ -1,7 +1,12 @@
 import express from 'express';
-const router = express.Router();
+import bodyParser from "body-parser";
+import multer from 'multer';
 
-router.get('/', (req, res) => {
+const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const upload = multer({ dest: 'uploads/' });
+
+router.get('/', async (req, res) => {
     let pActive = true;
 
     // Get information of user from session
@@ -19,37 +24,91 @@ router.get('/', (req, res) => {
         res.redirect('/');
         return;
     }
+
     res.render('seller/post-product',{
         pActive,
         layout: 'seller.handlebars'
     });
 });
 
-router.get('/post', (req, res) => {
+router.post('/',urlencodedParser, upload.single('img'), async(req, res) => {
     let pActive = true;
+    console.log(req.body);
+    console.log(req.file);
     res.render('seller/post-product',{
         pActive,
         layout: 'seller.handlebars'
     });
 });
 
-router.get('/additional', (req, res) => {
+router.get('/additional', async (req, res) => {
     let aActive = true;
+
+    // Get information of user from session
+    const user = req.session.authUser || 0;
+    if (!user) {
+        console.log("Please login first ! ");
+        res.redirect('/');
+        return;
+    }
+
+    // Get type of User: 1 -> Seller | 2 -> Bidder | 3 -> Admin
+    const isSeller = req.session.isSeller;
+    if (!isSeller) {
+        console.log("You don't have permission to access this page ! ");
+        res.redirect('/');
+        return;
+    }
+
     res.render('seller/additional',{
         aActive,
         layout: 'seller.handlebars'
     });
 });
 
-router.get('/selling', (req, res) => {
+router.get('/selling', async (req, res) => {
     let vActive = true;
+
+    // Get information of user from session
+    const user = req.session.authUser || 0;
+    if (!user) {
+        console.log("Please login first ! ");
+        res.redirect('/');
+        return;
+    }
+
+    // Get type of User: 1 -> Seller | 2 -> Bidder | 3 -> Admin
+    const isSeller = req.session.isSeller;
+    if (!isSeller) {
+        console.log("You don't have permission to access this page ! ");
+        res.redirect('/');
+        return;
+    }
+
     res.render('seller/selling',{
         vActive,
         layout: 'seller.handlebars'
     });
 });
-router.get('/sold', (req, res) => {
+router.get('/sold', async (req, res) => {
     let vActive = true;
+
+    // Get information of user from session
+    const user = req.session.authUser || 0;
+    if (!user) {
+        console.log("Please login first ! ");
+        res.redirect('/');
+        return;
+    }
+
+    // Get type of User: 1 -> Seller | 2 -> Bidder | 3 -> Admin
+    const isSeller = req.session.isSeller;
+    if (!isSeller) {
+        console.log("You don't have permission to access this page ! ");
+        res.redirect('/');
+        return;
+    }
+
     res.render('seller/sold',{
         vActive,
         layout: 'seller.handlebars'
