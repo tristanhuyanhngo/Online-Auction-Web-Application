8e758318b90a37fa361163f9d7d49b6b80a729e6
phucthaii1820 <62050136+phucthaii1820@users.noreply.github.com>
2022-01-12 21:23:13 +0700

    test login by gmail

--- auth/authG.js
+++ auth/authG.js
@@ -10,7 +10,7 @@ const ggStrategy = GoogleStrategy.Strategy;
 passport.use(new ggStrategy({
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: "http://horizon-online-auction.herokuapp.com/auth/google/callback",
         passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
